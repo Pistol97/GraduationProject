07d281cdde4d11e2b247fbfc9c5cc25838d4a81c
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2021-03-01 21:29:52 +0900

    PlayerMovement&Camera

---
+++ Assets/Scripts/MainCameraAction.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraAction : MonoBehaviour
+{
+    public GameObject player;
+
+    public float offsetX = 0f;
+    public float offsetY = 25f;
+    public float offsetZ = -35f;
+
+    Vector3 cameraPosition;
+
+    public float followSpeed = 2.0f;
+
+    public Camera cameraObj;
+
+    //void Update()
+    //{
+    //    RotateCamera();
+    //}
+
+    private void LateUpdate()
+    {
+        cameraPosition.x = player.transform.position.x + offsetX;
+        cameraPosition.y = player.transform.position.y + offsetY;
+        cameraPosition.z = player.transform.position.z + offsetZ;
+
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, followSpeed * Time.deltaTime);
+
+        //RotateCamera();
+    }
+
+    void RotateCamera()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            cameraObj.transform.RotateAround(player.transform.position,
+                                            Vector3.up,
+                                            Input.GetAxis("Mouse X") * followSpeed);
+            cameraObj.transform.RotateAround(player.transform.position,
+                                            Vector3.right,
+                                            -Input.GetAxis("Mouse Y") * followSpeed);
+        }
+    }
+}
