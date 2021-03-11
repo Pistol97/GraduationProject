beb99db624a9690e54bc311741dc88b87e897783
Pistol97 <ehfdl75@gmail.com>
2021-03-11 14:41:51 +0900

    Update process

---
+++ Assets/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public bool chase = false;
+
+    public Vector3 location;
+
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(chase)
+        {
+            agent.SetDestination(location);
+        }
+    }
+}
---
+++ Assets/Noise.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise : MonoBehaviour
+{
+    private SphereCollider sphereCollider;
+
+    private float noiseRadius;
+    public float NoiseRadius
+    {
+        set
+        {
+            noiseRadius = value;
+        }
+    }
+
+    private void Awake()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+    }
+
+    private void Update()
+    {
+        //sphereCollider.radius = noiseRadius;
+    }
+}
