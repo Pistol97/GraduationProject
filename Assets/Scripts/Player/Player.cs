f4f9210e1a5c90c6ba1e94d89b4b1fff110e1494
Pistol97 <ehfdl75@gmail.com>
2021-04-26 15:14:30 +0900

    add player footsteps sound & enemy ping sound

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
+    [SerializeField] private AudioClip[] footsteps;
 
-    [SerializeField]
-    private Slider Bar_Fear;
+    [SerializeField] private Slider Bar_Fear;
 
     [Header("플레이어 상태 변수")]
     [SerializeField]
@@ -15,10 +15,32 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float maxFearRange = 100;//최대 공포 수치, 게임 오버
 
+    private PlayerControl playerControl;
+
+    private float timer = 0;
+    private float wait = 0;
+    private void Awake()
+    {
+        playerControl = GetComponent<PlayerControl>();
+    }
+
     private void Start()
     {
         //공포 수치 초기화
         Bar_Fear.value = 0f;
+    }
+
+    private void Update()
+    {
+        if (playerControl.GetVelocitySpeed() != 0)
+        {
+            if (!GetComponent<AudioSource>().isPlaying && timer >= wait)
+            {
+                GetComponent<Player>().PlayWalkFootstep();
+                timer = 0f;
+            }
+            timer += Time.deltaTime;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,5 +57,15 @@ public class Player : MonoBehaviour
     {
         fearRange += 10;
         Bar_Fear.value = fearRange / maxFearRange;
+    }
+
+    public void PlayWalkFootstep()
+    {
+        int footstep = Random.Range(0, footsteps.Length - 1);
+
+        GetComponent<AudioSource>().clip = footsteps[footstep];
+        wait = footsteps[footstep].length + 0.3f;
+        GetComponent<AudioSource>().Play();
+        Debug.Log("Footsteps");
     }
 }
