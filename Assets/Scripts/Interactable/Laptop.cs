dff83cfd8775256b6c4721ac1394914a3d1d0ed3
Pistol97 <ehfdl75@gmail.com>
2021-04-29 03:51:31 +0900

    add laptop interaction & sound

---
+++ Assets/Scripts/Interactable/Laptop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laptop : MonoBehaviour, IInteractable
+{
+    private MeshRenderer renderer;
+    [SerializeField] private Material turnOn;
+
+    private void Awake()
+    {
+        renderer = GetComponent<MeshRenderer>();    
+    }
+
+    public void ObjectInteract()
+    {
+        renderer.material = turnOn;
+        gameObject.tag = "Untagged";
+        GetComponent<AudioSource>().Play();
+    }
+}
