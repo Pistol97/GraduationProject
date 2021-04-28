64224c4e0f05b43da20bb0db8551c7d7603301d9
Pistol97 <ehfdl75@gmail.com>
2021-04-29 03:25:50 +0900

    add Door sound & animation

--- Assets/Scripts/Interactable/Door.cs
+++ Assets/Scripts/Interactable/Door.cs
@@ -1,46 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
     private Animator animator;
-    private Animator handle;
-
-    [SerializeField] private AudioClip[] doorSound;
     private AudioSource audioSource;
 
-    private bool isActivate;
+    [SerializeField] private AudioClip[] doorSounds;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        handle = transform.GetChild(0).GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
-        isActivate = false;
     }
 
-    private void Update()
-    {
-        if (isActivate && !handle.GetBool("IsPull"))
-        {
-            Debug.Log("Door Open");
-            animator.SetBool("IsOpen", true);
-            audioSource.Play();
-            isActivate = false;
-        }
-
-        if(animator.GetCurrentAnimatorStateInfo(0).IsName("Gate_Close") && !audioSource.isPlaying)
-        {
-            audioSource.clip = doorSound[1];
-            audioSource.Play();
-        }
-    }
-
-    //인터페이스 함수
     public void ObjectInteract()
     {
-        handle.SetBool("IsPull", true);
-        isActivate = true;
-        audioSource.clip = doorSound[0];
+
+        if (!animator.GetBool("IsOpen"))
+        {
+            animator.SetBool("IsOpen", true);
+            audioSource.clip = doorSounds[0];
+            audioSource.Play();
+        }
+
+        else
+        {
+            animator.SetBool("IsOpen", false);
+            audioSource.clip = doorSounds[1];
+        }
     }
 }
