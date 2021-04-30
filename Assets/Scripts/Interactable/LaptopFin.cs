edec85c64b4a164b55bab764711e788e8ae3a42a
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2021-04-30 23:28:37 +0900

    WhiteLapTopAccessAdded

--- Assets/Scripts/Interactable/LaptopFin.cs
+++ Assets/Scripts/Interactable/LaptopFin.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 public class LaptopFin : MonoBehaviour,IInteractable
 {
     [Header("이벤트 메시지 작성")]
-    [SerializeField] private string message;
     [SerializeField] private GameObject finishMsg;
     [SerializeField] private Button finishButton;
     [SerializeField] private GameObject crosshair;
     public void ObjectInteract()
     {
+        if (!Gate.level1)
+            FindObjectOfType<EventMessage>().DisplayMessage("접근권한없음");
+        return;
+        if (!Gate.level2)
+            FindObjectOfType<EventMessage>().DisplayMessage("접근권한없음");
+        return;
+
         Time.timeScale = 0f;//시간 멈춤
 
         finishMsg.gameObject.SetActive(true);
