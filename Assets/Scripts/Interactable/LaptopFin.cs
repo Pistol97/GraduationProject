ef3acc6932388c62727023ec39a5d25da84e30ff
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2021-04-30 23:16:45 +0900

    WhiteLaptopUpdate&GameEnd

---
+++ Assets/Scripts/Interactable/LaptopFin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaptopFin : MonoBehaviour,IInteractable
+{
+    [Header("이벤트 메시지 작성")]
+    [SerializeField] private string message;
+    [SerializeField] private GameObject finishMsg;
+    [SerializeField] private Button finishButton;
+    [SerializeField] private GameObject crosshair;
+    public void ObjectInteract()
+    {
+        Time.timeScale = 0f;//시간 멈춤
+
+        finishMsg.gameObject.SetActive(true);
+        finishButton.gameObject.SetActive(true);
+        crosshair.gameObject.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+}
