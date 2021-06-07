5585d80d21e735fe3164892bf7ff804a4930c469
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2021-06-07 17:15:08 +0900

    QuestUpdate

---
+++ Assets/Scripts/Lobby/QuestDataController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestDataController : MonoBehaviour
+{
+    /// <summary>
+    /// 싱글턴
+    /// </summary>
+    private static QuestDataController instance;
+    public static QuestDataController GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = FindObjectOfType<QuestDataController>();
+
+            if (instance == null)
+            {
+                GameObject container = new GameObject("QuestDataController");
+
+                instance = container.AddComponent<QuestDataController>();
+            }
+        }
+        return instance;
+    }
+
+    //[SerializeField] private int[,] questProgress;
+
+    //[SerializeField] private int npcnum1;
+    //[SerializeField] private int npcnum2;
+    //[SerializeField] private int npcnum3;
+    //[SerializeField] private int npcquestnum1;
+    //[SerializeField] private int npcquestnum2;
+    //[SerializeField] private int npcquestnum3;
+
+    //private void Awake()
+    //{
+    //    questProgress = new int[2,2];
+    //}
+
+    //public void QuestSetComplete()
+    //{
+    //    PlayerPrefs.SetInt("QuestComplete", 0);
+    //}
+
+    //public void QuestSetFail()
+    //{
+    //    PlayerPrefs.SetInt("QuestComplete", 1);
+    //}
+
+    public void SetQuest(int _ox)
+    {
+        PlayerPrefs.SetInt("QuestComplete", _ox);
+    }
+
+    public int GetQuest()
+    {
+        return PlayerPrefs.GetInt("QuestComplete");
+    }
+
+    public void SetQuestItem(string _item)
+    {
+        PlayerPrefs.SetString("Item", _item);
+    }
+
+    public string GetQuestItem()
+    {
+        return PlayerPrefs.GetString("Item");
+    }
+}
--- Assets/Scripts/Lobby/QuestMgr.cs
+++ Assets/Scripts/Lobby/QuestMgr.cs
@@ -8,10 +8,10 @@ public class QuestMgr : MonoBehaviour
     [System.Serializable]
     public class QuestDataProperty
     {
-        //public int npcID;
         public int npcQuestNum;
         public Sprite sprite;
         public string description;
+        public string questItem;
         public string reward;
     }
 
@@ -19,59 +19,122 @@ public class QuestMgr : MonoBehaviour
     public Text npcQuestNum;
     public Text description;
     public Button back;
-
-    public int testquest;
-
-    public List<QuestDataProperty> questData;
-
-    public Dictionary<int,QuestDataProperty> npcQuestData;
     
     public List<QuestDataProperty> npc1Quest;
     public List<QuestDataProperty> npc2Quest;
     public List<QuestDataProperty> npc3Quest;
 
+    //public List<List<QuestDataProperty>> Quest;
+
     public int npc1QuestNum = 0;
     public int npc2QuestNum = 0;
     public int npc3QuestNum = 0;
 
-    [System.Serializable]
-    public class SerializeDicString : SerializeDictionary<string, string> { }
+    public int currentNpc;
 
-    public SerializeDicString questDictionary = new SerializeDicString();
+    public bool test=false;
+
+    Dictionary<int, List<QuestDataProperty>> testQuest;
+
+    private void Awake()
+    {
+        QuestDataController.GetInstance().SetQuest(0);
+
+        //Quest = new List<List<QuestDataProperty>>();
+        //Quest.Add(npc1Quest);
+        //Quest.Add(npc2Quest);
+        //Quest.Add(npc3Quest);
+
+        //testQuest = new Dictionary<int, List<QuestDataProperty>>();
+        //testQuest.Add(1, npc1Quest);
+        //testQuest.Add(2, npc2Quest);
+        //testQuest.Add(3, npc3Quest);
+    }
 
     private void Update()
     {
-        //if (testquest >= 0 && testquest <= questData.Count)
+        //for(int i=0;i<Quest.Count;i++)
         //{
-        //    UpdateContant(testquest - 1);
+        //    for(int j =0; j<Quest[i].Count;j++)
+        //    {
+        //        Debug.Log("퀘스트" + i+" = " + Quest[i][j].ToString());
+        //    }
         //}
+
+        //foreach(KeyValuePair<int,QuestDataProperty> pair in testQuest)
+        //{
+        //}
+
+        if (test == true)
+            QuestDataController.GetInstance().SetQuest(1);
+
+        QuestSuccess();
     }
 
-    private void UpdateContant(int npcID)
+    public void Show()
     {
-        npc.sprite = questData[npcID].sprite;
-        npcQuestNum.text = questData[npcID].npcQuestNum.ToString();
-        description.text = questData[npcID].description;
     }
 
     public void NPC1Quest()
     {
+        currentNpc = 1;
+
         npc.sprite = npc1Quest[npc1QuestNum].sprite;
         npcQuestNum.text = npc1Quest[npc1QuestNum].npcQuestNum.ToString();
         description.text = npc1Quest[npc1QuestNum].description;
+
+        string questItem = npc1Quest[npc1QuestNum].questItem;
+        QuestDataController.GetInstance().SetQuestItem(questItem);
     }
 
     public void NPC2Quest()
     {
+        currentNpc = 2;
+
         npc.sprite = npc2Quest[npc2QuestNum].sprite;
         npcQuestNum.text = npc2Quest[npc2QuestNum].npcQuestNum.ToString();
         description.text = npc2Quest[npc2QuestNum].description;
+
+        string questItem = npc2Quest[npc2QuestNum].questItem;
+        QuestDataController.GetInstance().SetQuestItem(questItem);
     }
 
     public void NPC3Quest()
     {
+        currentNpc = 3;
+
         npc.sprite = npc3Quest[npc3QuestNum].sprite;
         npcQuestNum.text = npc3Quest[npc3QuestNum].npcQuestNum.ToString();
         description.text = npc3Quest[npc3QuestNum].description;
+
+        string questItem = npc3Quest[npc3QuestNum].questItem;
+        QuestDataController.GetInstance().SetQuestItem(questItem);
+    }
+
+    private void QuestSuccess()
+    {
+        if (QuestDataController.GetInstance().GetQuest()==0)
+            return;
+
+        switch(currentNpc)
+        {
+            case 1:
+                npc1QuestNum += 1;
+                test = false;
+                QuestDataController.GetInstance().SetQuest(0);
+                break;
+            case 2:
+                npc2QuestNum += 1;
+                test = false;
+                QuestDataController.GetInstance().SetQuest(0);
+                break;
+            case 3:
+                npc3QuestNum += 1;
+                test = false;
+                QuestDataController.GetInstance().SetQuest(0);
+                break;
+            default:
+                break;
+        }
     }
 }
