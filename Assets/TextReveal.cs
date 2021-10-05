0facde33c07aed01d36d8c737b9a082a4bf4aef0
Pistol97 <ehfdl75@gmail.com>
2021-10-05 16:48:19 +0900

    add text reveal effect

---
+++ Assets/TextReveal.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TextReveal : MonoBehaviour
+{
+    private TextMeshPro tm;
+    private Sonar sonar;
+
+    private void Awake()
+    {
+        tm = GetComponentInChildren<TextMeshPro>();
+        tm.color = new Color(tm.color.r, tm.color.g, tm.color.b, 0f);
+    }
+
+    private void Update()
+    {
+        if(sonar)
+        {
+            tm.color = new Color(tm.color.r, tm.color.g, tm.color.b, sonar.dissappear);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Sonar"))
+        {
+            Debug.Log("Text revealing");
+            sonar = other.GetComponent<Sonar>();
+        }
+    }
+}
