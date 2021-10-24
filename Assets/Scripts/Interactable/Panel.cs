f3c989cee15a32da849c875fe7da24c61600be71
Garden1298 <54656129+Garden1298@users.noreply.github.com>
2021-10-24 17:58:40 +0900

    Panel key Added

---
+++ Assets/Scripts/Interactable/Panel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel : MonoBehaviour, IInteractable
+{
+    [SerializeField] private Inventory inven;
+
+    //인터페이스 함수
+    public void ObjectInteract()
+    {
+        if(inven.FindItemWithName("PanelKey"))
+        {
+            inven.UseItemWithName("PanelKey");
+        }
+    }
+}
