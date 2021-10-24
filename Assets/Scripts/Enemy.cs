cc916d995c8e92805baddc512fcd046f8bfb94d6
Pistol97 <ehfdl75@gmail.com>
2021-10-24 16:47:13 +0900

    fix merge

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -66,8 +66,6 @@ public class Enemy : MonoBehaviour
 
         enemySightLevel = NoiseSystemManager.GetInstance().GetFearLevel();
         SightLevel();
-
-        ChangeIdle();
     }
 
     private void FixedUpdate()
