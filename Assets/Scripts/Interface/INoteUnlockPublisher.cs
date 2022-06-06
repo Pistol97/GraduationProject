32aa82c46210c143b33070947c8fc4479204de6c
Pistol97 <54612434+Pistol97@users.noreply.github.com>
2022-06-06 09:40:48 +0900

    change interface name

---
+++ Assets/Scripts/Interface/INoteUnlockPublisher.cs
@@ -0,0 +1,7 @@
+﻿//노트 언락을 알리기 위한 Subject
+public interface INoteUnlockPublisher
+{
+    void AddObserver(INoteUnlockObserver observer);
+    void DeleteObserver(INoteUnlockObserver observer);
+    void NotifyObserver();
+}
--- Assets/Scripts/Interface/NoteUnlockObserver.cs
+++
@@ -1,7 +0,0 @@
-﻿//퍼블리셔로부터 알림을 받는 인터페이스
-//Observer Pattern
-public interface NoteUnlockObserver
-{
-    void UpdateUnlock(int noteNumber);
-
-}
